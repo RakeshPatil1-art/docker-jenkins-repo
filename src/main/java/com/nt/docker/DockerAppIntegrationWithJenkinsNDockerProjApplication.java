2c1028e2ac3fc5a417e9e4f87a4478b48c93f333
RakeshPatil1-art <rrsonawane20@gmail.com>
2020-07-17 11:16:28 -0700

    First Commit

--- src/main/java/com/nt/docker/DockerAppIntegrationWithJenkinsNDockerProjApplication.java
+++ src/main/java/com/nt/docker/DockerAppIntegrationWithJenkinsNDockerProjApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DockerAppIntegrationWithJenkinsNDockerProjApplication {
 	
 	
-	@GetMapping("Wish")
+	@GetMapping("/Wish")
 	public String wishMsg() {
 		return "Good Morning";
 	}
